a9b1d434f2354181898ade6a37ef855a3388fd75
Asad Khan <asad219@hotmail.com>
2024-04-22 13:34:17 +0400

    configured ios firebase

--- lib/global.dart
+++ lib/global.dart
@@ -6,12 +6,13 @@ class Global {
   static late StorageService storageService;
   static Future init() async {
     WidgetsFlutterBinding.ensureInitialized();
-    await Firebase.initializeApp(
-        options: const FirebaseOptions(
-            apiKey: "AIzaSyD3nkTgehwADkeF13QhpgVOwkk6Jlnwa4E",
-            appId: '790597821762:android:6ca4865b4e7cf35c9314db',
-            messagingSenderId: '790597821762',
-            projectId: 'ulearing-app-eb4b2'));
+    await Firebase.initializeApp();
+    // (
+    //     options: const FirebaseOptions(
+    //         apiKey: "AIzaSyD3nkTgehwADkeF13QhpgVOwkk6Jlnwa4E",
+    //         appId: '790597821762:android:6ca4865b4e7cf35c9314db',
+    //         messagingSenderId: '790597821762',
+    //         projectId: 'ulearing-app-eb4b2'));
 
     storageService = await StorageService().init();
   }
